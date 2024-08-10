d0613d7a09cd86030e72b93da6b9a872d949433f
Krzysztof <krzys.zolnowski@gmail.com>
2024-08-10 22:54:37 +0200

    4 conditions done

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryGreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        int b = 16;
+
+        int greatest = b > 0 ? 10 : 23;
+        System.out.println(greatest);
+
+
     }
 }
