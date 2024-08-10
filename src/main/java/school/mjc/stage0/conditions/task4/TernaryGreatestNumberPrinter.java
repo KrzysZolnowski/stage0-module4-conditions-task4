f8b73455b57b8905a62a1967c74d78db7aa1b087
Krzysztof <krzys.zolnowski@gmail.com>
2024-08-10 23:09:00 +0200

    4 conditions fixed firs time

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
@@ -3,10 +3,8 @@ package school.mjc.stage0.conditions.task4;
 public class TernaryGreatestNumberPrinter {
     public void printGreatest(int first, int second) {
         int b = 16;
-
-        int greatest = b > 0 ? 10 : 23;
-        System.out.println(greatest);
-
+        int z = b > first ? 10 : (second > b ? second : 23);
+        System.out.println(z);
 
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -3,5 +3,6 @@ package school.mjc.stage0.conditions.task4;
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
         int biggest = (first > second) ? (first > third ? first : third): (second > third ? second : third);
+        System.out.println(biggest);
     }
 }
